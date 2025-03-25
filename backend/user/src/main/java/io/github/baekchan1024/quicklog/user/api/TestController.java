28713ab7bf8ed2a555ae11cc1369f1747d45758f
baekchan1024 <baekchan1024@gmail.com>
2025-03-25 18:37:43 +0900

    [feat] 1차 백엔드 모듈 구조 구현

---
+++ backend/user/src/main/java/io/github/baekchan1024/quicklog/user/api/TestController.java
@@ -0,0 +1,13 @@
+package io.github.baekchan1024.quicklog.user.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello world";
+    }
+}
